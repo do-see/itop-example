b47437db83f46b048358da5f29a5368e90aa9430
Dominik Seemiller <dominik.seemiller@xitaso.com>
2022-12-05 22:06:36 +0100

    mvp - working version

---
+++ README.md
@@ -0,0 +1,2 @@
+Example docker-compose for itop.
+This is only a playground, do not use for production.
---
+++ setup-dirs.sh
@@ -0,0 +1,5 @@
+mkdir -p ./itop-data/conf
+mkdir -p ./itop-data/data
+mkdir -p ./itop-data/env-production
+mkdir -p ./itop-data/log
+mkdir -p ./mysql-data
